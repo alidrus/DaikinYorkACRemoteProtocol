7d808fa745875019644191759799e56e623a14b1
Abu Bakar Al-Idrus <alidrus@gmail.com>
2019-08-10 17:44:16 +0800

    Changed class name to DaikinDGS01Remote. Turns out TH209 was just some number printed on the back of the remote controller. Also fixed some typedefs, cleaned up some private functions, properties and started coding the raw pulse timing reconstruction function (WIP).

--- DaikinDGS01Remote.cpp
+++ DaikinDGS01Remote.cpp
@@ -1,40 +1,164 @@
-// vim: syntax=c expandtab tabstop=4 shiftwidth=4 softtabstop=4:
+// vim: syntax=c autoindent smartindent expandtab tabstop=4 shiftwidth=4 softtabstop=4:
 
-#include <Arduino.h>
-#include "DaikinTH209Remote.h"
+#include "DaikinDGS01Remote.h"
 
-DaikinTH209Remote::DaikinTH209Remote() {
+// Class Constructor
+DaikinDGS01Remote::DaikinDGS01Remote() {
 }
 
-void DaikinTH209Remote::setOperationMode(operation_mode_t mode) {
+// Set the operation mode of bit
+void DaikinDGS01Remote::setOperationMode(operation_mode_t operationMode) {
+    settings.operationMode = operationMode;
 }
 
-void DaikinTH209Remote::setFanMode(fan_mode_t fanMode) {
+void DaikinDGS01Remote::setFanMode(fan_mode_t fanMode) {
+    settings.fanMode = fanMode;
 }
 
-void DaikinTH209Remote::setTime(time_struct_t time) {
+void DaikinDGS01Remote::setTime(time_struct_t currentTime) {
+    if (currentTime.hour <= 23 && currentTime.hour >= 0
+        && currentTime.minute <= 59 && currentTime.minute >= 0)
+    {
+        settings.currentTime = currentTime;
+    }
 }
 
-void DaikinTH209Remote::setOnTimer(timer_struct_t onTimer) {
+void DaikinDGS01Remote::setOnTimer(timer_struct_t onTimer) {
+    if (onTimer.hour <= 23 && onTimer.hour >= 0
+        && (onTimer.halfHour || !onTimer.halfHour))
+    {
+        settings.onTimer = onTimer;
+    }
 }
 
-void DaikinTH209Remote::setOffTimer(timer_struct_t offTimer) {
+void DaikinDGS01Remote::setOffTimer(timer_struct_t offTimer) {
+    if (offTimer.hour <= 23 && offTimer.hour >= 0)
+    {
+        settings.offTimer = offTimer;
+    }
 }
 
-void DaikinTH209Remote::setTemperature(int temperature) {
+void DaikinDGS01Remote::setTemperature(int temperature) {
+    if (temperature >= 16 && temperature <= 30)
+    {
+        settings.temperature = temperature;
+    }
 }
 
-void DaikinTH209Remote::setSleep(bool active) {
+void DaikinDGS01Remote::setSleep(bool active) {
+    settings.sleep = active;
 }
 
-void DaikinTH209Remote::setSwing(bool active) {
+void DaikinDGS01Remote::setSwing(bool active) {
+    settings.sleep = active;
 }
 
-unsigned int *DaikinTH209Remote::getRawTimings(bool powerButton) {
+unsigned int *DaikinDGS01Remote::getRawTimings(bool powerButton = false) {
+    // Storage for the raw timings (in microseconds) of the pulses we are
+    // sending to the AC.
+    unsigned int rawTimings[137];
+
+    // This will keep track of the current index of the rawTimings array.
+    unsigned int timingPosition = 0;
+
+    // A temporary place to hold the data for the current nibble (nibble = 4
+    // bits of data, or half a byte ... *chomp* LOL).
+    byte tmpNibble;
+
+    // Copy the powerButton boolean flag passed to us by the caller into the AC
+    // settings structure.
+    settings.powerButton = powerButton;
+
+    // The "start" transmission signal comprises of alternating 3 pulses and 3
+    // pauses. As far as I know this does not represent any binary value. It is
+    // probably some kind of transmission start marker which confused the
+    // IRremote library because this header has 3 pulses with pauses in between
+    // and not just 1 pulse like in other protocols.
+    for (int i = 0; i < sizeof(startTransmission); i++)
+    {
+        rawTimings[timingPosition++] = startTransmission[i];
+    }
+
+    //
+    // The next 64 pulses (each followed by either a long or short pause)
+    // represent binary data sent to the AC. A pulse followed by a long pause
+    // represents binary 1 whereas a pulse followed by a short pause represents
+    // a binary 0.
+    //
+
+    // This seems to be the binary data header which is 8 bits long. It seems
+    // to be a binary representation of the ASCII character 'h' (probably just
+    // short for "header")
+    for (int i = 0; i < 8; i++)
+    {
+        // Temporarily store the right most bit after shifting all bits to the
+        // right i times.
+        tmpNibble = (((byte) settings.header) >> i) & 0b00000001;
+
+        // Added it to the timing
+        rawTimings[timingPosition++] = markLength;
+        rawTimings[timingPosition++] = tmpNibble ? spaceLength0 : spaceLength1;
+    }
+
+    // 4-bit operation mode (4 pulses + 4 pauses)
+    for (int i = 0; i < 4; i++)
+    {
+        tmpNibble = (((byte) settings.operationMode) >> i) & 0b00000001;
+
+        rawTimings[timingPosition++] = markLength;
+        rawTimings[timingPosition++] = tmpNibble ? spaceLength0 : spaceLength1;
+    }
+
+    // 4-bit fan mode (4 pulses + 4 pauses)
+    for (int i = 0; i < 4; i++)
+    {
+        tmpNibble = (((byte) settings.fanMode) >> i) & 0b00000001;
+
+        rawTimings[timingPosition++] = markLength;
+        rawTimings[timingPosition++] = tmpNibble ? spaceLength0 : spaceLength1;
+    }
+
+    // 4-bit time minute right digit (1s) (4 pulses + 4 pauses)
+    tmpNibble = (byte) (settings.currentTime.minute % 10);
+    tmpNibble = reverseNibble(tmpNibble);
+    for (int i = 0; i < 4; i++)
+    {
+        rawTimings[timingPosition++] = markLength;
+        rawTimings[timingPosition++] = ((tmpNibble >> i) & 0b00000001) ? spaceLength0 : spaceLength1;
+    }
+
+    // 4-bit time minute left digit (10s) (4 pulses + 4 pauses)
+    tmpNibble = (byte) (settings.currentTime.minute / 10);
+    tmpNibble = reverseNibble(tmpNibble);
+    for (int i = 0; i < 4; i++)
+    {
+        rawTimings[timingPosition++] = markLength;
+        rawTimings[timingPosition++] = ((tmpNibble >> i) & 0b00000001) ? spaceLength0 : spaceLength1;
+    }
+
+    // 4-bit time hour right digit (1s) (4 pulses + 4 pauses)
+    tmpNibble = (byte) (settings.currentTime.hour % 10);
+    tmpNibble = reverseNibble(tmpNibble);
+    for (int i = 0; i < 4; i++)
+    {
+        rawTimings[timingPosition++] = markLength;
+        rawTimings[timingPosition++] = ((tmpNibble >> i) & 0b00000001) ? spaceLength0 : spaceLength1;
+    }
+
+    // 4-bit time hour left digit (10s) (4 pulses + 4 pauses)
+    tmpNibble = (byte) (settings.currentTime.hour / 10);
+    tmpNibble = reverseNibble(tmpNibble);
+    for (int i = 0; i < 4; i++)
+    {
+        rawTimings[timingPosition++] = markLength;
+        rawTimings[timingPosition++] = ((tmpNibble >> i) & 0b00000001) ? spaceLength0 : spaceLength1;
+    }
+
+    return rawTimings;
 }
 
 // reverse the bit order of a nibble
-byte DaikinTH209Remote::reverseNibble (byte nibble, bool leftNibble = false)
+byte DaikinDGS01Remote::reverseNibble(byte nibble, bool leftNibble = false)
 {
     if (!leftNibble)
     {
--- DaikinDGS01Remote.h
+++ DaikinDGS01Remote.h
@@ -1,37 +1,67 @@
-// vim: syntax=c expandtab tabstop=4 shiftwidth=4 softtabstop=4:
+// vim: syntax=c autoindent smartindent expandtab tabstop=4 shiftwidth=4 softtabstop=4:
 
-#ifndef DAIKIN_TH209_REMOTE_H
-#define DAIKIN_TH209_REMOTE_H
+/*
+ * The ifndef tells the compiler to only compile the following lines if it has
+ * not been done before
+ */
+#ifndef DAIKIN_DGS01_REMOTE_H
+#define DAIKIN_DGS01_REMOTE_H
 
+/*
+ * This include is needed so that Arduino data types are recognized by the
+ * compiler.
+ */
+#include <Arduino.h>
+
+/*
+ * The following typedef blocks define custom variable types which hold data
+ * structures specific to this library.
+ */
+
+// The time_struct_t type is used to define a variable for storing the hour and
+// minute of the current time. This will be used to send the date/time to the
+// AC indoor unit.
 typedef struct {
-    int hour;
-    int minute;
+    unsigned int hour;
+    unsigned int minute;
 }  time_struct_t;
 
+// The timer_struct_t type is used to define a variable to hold the auto on/off
+// data to send to the AC indoor unit. As with the remote controller, you can
+// only define a time with 30 minute increments.
 typedef struct {
-    int hour;
+    unsigned int hour;
     bool halfHour;
     bool active;
 } timer_struct_t;
 
+// The operation_mode_t type is used to define a variable to hold the desired
+// operation mode of the AC.
 typedef enum {
-    OPERATION_MODE_COOL,
-    OPERATION_MODE_DRY,
-    OPERATION_MODE_FAN
+    OPERATION_MODE_DRY  = 0b1000,
+    OPERATION_MODE_COOL = 0b0100,
+    OPERATION_MODE_FAN  = 0b0010
 } operation_mode_t;
 
+// The fan_mode_t type is used to define a variable to hold the fan mode of the
+// AC.
 typedef enum {
-    FAN_MODE_MANUAL_SPEED_1,
-    FAN_MODE_MANUAL_SPEED_2,
-    FAN_MODE_MANUAL_SPEED_3,
-    FAN_MODE_AUTO,
-    FAN_MODE_TURBO,
-    FAN_MODE_QUIET
+    FAN_MODE_MANUAL_SPEED_1 = 0b0001,
+    FAN_MODE_MANUAL_SPEED_2 = 0b0010,
+    FAN_MODE_MANUAL_SPEED_3 = 0b0100,
+    FAN_MODE_AUTO           = 0b1000,
+    FAN_MODE_QUIET          = 0b1001,
+    FAN_MODE_TURBO          = 0b1100
 } fan_mode_t;
 
+// The ac_settings_t type is used to define a variable to hold all the current
+// setting of the air conditioner.
 typedef struct {
+    // Header
+    const char header = 'h';
+
     // Operation mode
-    operation_mode_t mode = OPERATION_MODE_COOL;
+    operation_mode_t operationMode = OPERATION_MODE_COOL;
 
     // Fan Mode
     fan_mode_t fanMode = FAN_MODE_AUTO;
@@ -51,7 +81,9 @@ typedef struct {
 
     // Off Timer { hour, halfHour, active }
     timer_struct_t offTimer = {
-        0, false, false
+        0,
+        false,
+        false
     };
 
     // Temperature Setting
@@ -67,31 +99,18 @@ typedef struct {
     bool powerButton = false;
 } ac_settings_t;
 
-typedef struct {
-    const unsigned int header[] = {
-        9788, 9676, 9812, 9680, 4652, 2408
-    };
-
-    unsigned int data[128] = {
-    };
-
-    const unsigned int endTransmission[] = {
-        368, 20340, 4624
-    };
-} raw_pulses_t;
-
-class DaikinTH209Remote
+class DaikinDGS01Remote
 {
     public:
 
         // Constructor
-        DaikinTH209Remote();
+        DaikinDGS01Remote();
 
-        void setOperationMode(operation_mode_t mode);
+        void setOperationMode(operation_mode_t operationMode);
 
         void setFanMode(fan_mode_t fanMode);
 
-        void setTime(time_struct_t time);
+        void setTime(time_struct_t currentTime);
 
         void setOnTimer(timer_struct_t onTimer);
 
@@ -103,25 +122,36 @@ class DaikinTH209Remote
 
         void setSwing(bool active);
 
-        unsigned int *getRawTimings(bool powerButton);
-
+        unsigned int *getRawTimings(bool powerButton = false);
 
     private:
-
-        byte reverseNibbleLookup[] = {
+        byte reverseNibbleLookup[16] = {
             0b0000, 0b1000, 0b0100, 0b1100, 0b0010, 0b1010, 0b0110, 0b1110,
             0b0001, 0b1001, 0b0101, 0b1101, 0b0011, 0b1011, 0b0111, 0b1111
         };
 
-        byte reverseNibble(byte nibble, bool leftNibble);
+        byte reverseNibble(byte nibble, bool leftNibble = false);
 
         // Mark and space lengths
         const unsigned int markLength   = 368;
         const unsigned int spaceLength0 = 368;
         const unsigned int spaceLength1 = 944;
 
+        // Start transmission timings
+        const unsigned int startTransmission[6] = {
+            9788, 9676, 9812, 9680, 4652, 2408
+        };
+
+        // Stop transmission timings
+        const unsigned int stopTransmission[3] = {
+            368, 20340, 4624
+        };
+
         // Air conditioner settings
-        AirCondSettings settings;
-}
+        ac_settings_t settings;
+
+        // Raw pulse timings
+        //raw_timing_t rawTimings;
+};
 
 #endif
